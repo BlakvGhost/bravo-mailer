20fa6222baeec28ab0b320fb1968399966ca1b11
Kabirou ALASSANE <kabirou2001@gmail.com>
2023-07-08 07:27:06 +0100

    Welcome: Operational presentation page

--- views/index.php
+++ views/index.php
@@ -5,11 +5,15 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Bravo-mailer is a project, a mini web platform for sending mail using an API provided.">
+    <meta name="keywords" content="Bravo, mailer, smtp, sending-mail,kabirou,alassane">
+    <meta name="author" content="Kabirou ALASSANE">
+    <meta name="robots" content="index, follow">
     <title>Welcome {{ $pageTitle }} | Bravo-Mailer</title>
     <link rel="favicon" type="image/ico" href="/favicon.ico">
 </head>
 
-<body>
+<body style="overflow-x:hidden;">
     <header>
         <?php require_once 'partials/header.php' ?>
     </header>
