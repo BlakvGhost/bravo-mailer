3dd0416fde65e7917971d492fae6d7e2830cd9d9
Kabirou ALASSANE <kabirou2001@gmail.com>
2023-07-08 06:20:31 +0100

    Adaptation of the routing between the two versions of bravo

--- routes/web.php
+++ routes/web.php
@@ -6,11 +6,5 @@ use App\Controllers\WelcomeController;
 use Juste\Facades\Routes\Route;
 
 Route::get("/", [WelcomeController::class, 'welcome'])->name('welcome');
-Route::resource('password', WelcomeController::class);
-
-
-Route::group(function () {
-    
-})->middlewares(['auth']);
 
 require_once 'api.php';
