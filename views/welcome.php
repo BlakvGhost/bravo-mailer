5fc0a1de22c13e5ccd9fd8669b1909096a66c83e
Kabirou ALASSANE <kabirou2001@gmail.com>
2023-07-08 06:56:46 +0100

    Welcome: Home page update

--- views/welcome.php
+++ views/welcome.php
@@ -1,1 +1,37 @@
-<h1 style="text-align: center;">Welcome To {{ $appName }} <?= route('api') ?></h1>
+<h1 style="text-align: center;">Welcome To {{ $appName }}</h1>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Bravo-mailer.md</title>
+    <style></style>
+</head>
+
+<body id="preview">
+    <h1 class="code-line" data-line-start="0" data-line-end="1"><a id="Bravomailer_0"></a>Introduction</h1>
+    <p class="has-line-data" data-line-start="2" data-line-end="4">Bravo-mailer is a project, a mini web platform for sending mail using an API provided.<br>
+        The idea came to me when I was developing my Portfolio using front-end technologies, i.e. my <a href="https://kabirou-alassane.com">portfolio</a> should be totally static without any recourse to a backend, so I encountered difficulties when sending mails at the level of my contact form, I tried to use the famous <a href="http://formspree.io">formspree.io</a> but over time when I changed my domain name I had some problems. So, I decided to write my own platform for managing and sending emails, in the form of an API.</p>
+    <p class="has-line-data" data-line-start="5" data-line-end="6">Now, with this project which is not yet graphical, but in REST API, I can communicate by SMTP with my mail server and send mails from my certified domains without worries.</p>
+    <p class="has-line-data" data-line-start="7" data-line-end="8">The other information is that I used my <code>personal mini PHP framework</code> that I call <a href="https://github.com/BlakvGhost/Bravo">BRAVO</a> (although this is the first version, not yet very efficient, but hey, it works, and I will certainly update it soon) .</p>
+    <h1 class="code-line" data-line-start="9" data-line-end="10"><a id="Documentation_9"></a>Documentation:</h1>
+    <p class="has-line-data" data-line-start="11" data-line-end="12">For soon</p>
+    <h1 class="code-line" data-line-start="13" data-line-end="14"><a id="How_to_use_13"></a>How to use:</h1>
+    <p class="has-line-data" data-line-start="14" data-line-end="15">Below is an example of using the API</p>
+    <p class="has-line-data" data-line-start="16" data-line-end="17"><img src="guide-0.PNG" alt="EXAMPLE OF API USAGE"></p>
+    <p class="has-line-data" data-line-start="18" data-line-end="19">All project code is based on my <a href="https://github.com/BlakvGhost/Bravo">BRAVO</a> v1.0.0 framework, so please do a <code>composer install</code> to see this code.</p>
+    <h2 class="code-line" data-line-start="20" data-line-end="21"><a id="Authors_20"></a>Authors</h2>
+    <ul>
+        <li class="has-line-data" data-line-start="22" data-line-end="24"><a href="https://github.com/BlakvGhost">@BlakvGhost</a></li>
+    </ul>
+    <h2 class="code-line" data-line-start="24" data-line-end="25"><a id="License_24"></a>License</h2>
+    <p class="has-line-data" data-line-start="26" data-line-end="29"><a href="https://choosealicense.com/licenses/mit/"><img src="https://img.shields.io/badge/License-MIT-green.svg" alt="MIT License"></a><br>
+        <a href="https://opensource.org/licenses/"><img src="https://img.shields.io/badge/License-GPL%20v3-yellow.svg" alt="GPLv3 License"></a><br>
+        <a href="http://www.gnu.org/licenses/agpl-3.0"><img src="https://img.shields.io/badge/license-AGPL-blue.svg" alt="AGPL License"></a>
+    </p>
+    <h2 class="code-line" data-line-start="30" data-line-end="31"><a id="Support_30"></a>Support</h2>
+    <p class="has-line-data" data-line-start="32" data-line-end="33">For support, email me on <a href="/cdn-cgi/l/email-protection#f19a909398839e84c3c1c1c0b1969c90989ddf929e9c"><span class="__cf_email__" data-cfemail="2b404a494259445e191b1b1a6b4c464a424705484446">[email&#160;protected]</span></a>.</p>
+    <script data-cfasync="false" src="/cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script>
+</body>
+
+</html>
